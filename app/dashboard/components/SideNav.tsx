0d7ba1dae69c6d7a3e9f4cf03ae75076b2dbc216
akhatarmourad <mourad.akhatar01@gmail.com>
2024-10-01 22:20:28 +0100

    Initial Commit

--- app/dashboard/components/SideNav.tsx
+++ app/dashboard/components/SideNav.tsx
@@ -17,12 +17,12 @@ export const SideBar = ({ className }: { className?: string }) => {
 		<div className={className}>
 			<div
 				className={cn(
-					"h-full w-full lg:w-96 lg:p-10 space-y-5 lg:border-r flex flex-col "
+					"h-full w-full lg:w-72 lg:p-10 space-y-5 lg:border-r flex flex-col "
 				)}
 			>
 				<div className="flex-1 space-y-5">
 					<div className="flex items-center gap-2 flex-1">
-						<h1 className="text-3xl font-bold">Daily Todo</h1>
+						<h1 className="text-3xl font-bold">Akhatar M.</h1>
 
 						<ModeToggle />
 					</div>
